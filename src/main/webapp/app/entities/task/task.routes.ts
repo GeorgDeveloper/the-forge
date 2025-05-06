0d879884725394d23710e21d2e87dc9edf39a6ad
GeorgDeveloper <ifreyd@yandex.ru>
2025-05-06 06:59:26 +0300

    update employee

--- src/main/webapp/app/entities/task/task.routes.ts
+++ src/main/webapp/app/entities/task/task.routes.ts
@@ -4,8 +4,10 @@ import { UserRouteAccessService } from 'app/core/auth/user-route-access.service'
 import { ASC } from 'app/config/navigation.constants';
 import TaskResolve from './route/task-routing-resolve.service';
 
+// Определение маршрутов для компонентов задач
 const taskRoute: Routes = [
   {
+    // Маршрут для отображения списка задач
     path: '',
     loadComponent: () => import('./list/task.component').then(m => m.TaskComponent),
     data: {
@@ -14,6 +16,7 @@ const taskRoute: Routes = [
     canActivate: [UserRouteAccessService],
   },
   {
+    // Маршрут для отображения деталей задачи
     path: ':id/view',
     loadComponent: () => import('./detail/task-detail.component').then(m => m.TaskDetailComponent),
     resolve: {
@@ -22,6 +25,7 @@ const taskRoute: Routes = [
     canActivate: [UserRouteAccessService],
   },
   {
+    // Маршрут для создания новой задачи
     path: 'new',
     loadComponent: () => import('./update/task-update.component').then(m => m.TaskUpdateComponent),
     resolve: {
@@ -30,6 +34,7 @@ const taskRoute: Routes = [
     canActivate: [UserRouteAccessService],
   },
   {
+    // Маршрут для редактирования существующей задачи
     path: ':id/edit',
     loadComponent: () => import('./update/task-update.component').then(m => m.TaskUpdateComponent),
     resolve: {
